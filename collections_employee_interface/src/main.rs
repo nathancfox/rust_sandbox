3b402c5e0ff2c0a61ead579f56582a6675237984
Nathan Fox <nchristopherfox@gmail.com>
2021-08-05 17:11:24 -0400

    Refactor and add placeholder functions.
    Refactored the command input from a fragile substring read
    to a split to string vector operation followed by a match
    of the first word. Also added a catch for invalid commands
    and an option to reprint valid commands.

--- collections_employee_interface/src/main.rs
+++ collections_employee_interface/src/main.rs
@@ -2,17 +2,21 @@ use std::io::{self, Write};
 
 fn main() {
     println!("Welcome to the Employee Database Interface.");
-    println!("\nValid commands are:");
-    println!("    add NAME to DEPT : Add an employee to a department.");
-    println!("    show DEPT        : Show all employees in a department.");
-    println!("    quit             : Quit this session.\n");
+    print_valid_commands();
     loop {
         let command = get_sentence();
-        let command_length = command.chars().count();
-        if command_length >= 4 {
-            if &command[..4] == "quit" {
-                break
-            }
+        let split_command: Vec<&str> = command.split(" ").collect();
+        if split_command.len() == 0 {
+            continue
+        }
+        match split_command[0] {
+            "add" => add_employee(split_command),
+            "show" => show_department(split_command),
+            "commands" => print_valid_commands(),
+            "quit" => break,
+            _ => {
+                println!("Not a valid command. Enter \"commands\" to see a list of valid commands.");
+            },
         }
     }
 }
@@ -33,4 +37,19 @@ fn get_sentence() -> String {
         break input;
     };
     return sentence;
+}
+
+fn add_employee(split_command: Vec<&str>) {
+    println!("Adding employee with command \"{}\"", split_command.join(" "));
+}
+
+fn show_department(split_command: Vec<&str>) {
+    println!("Showing employees of department with command \"{}\"", split_command.join(" "));
+}
+
+fn print_valid_commands() {
+    println!("\nValid commands are:");
+    println!("    add NAME to DEPT : Add an employee to a department.");
+    println!("    show DEPT        : Show all employees in a department.");
+    println!("    quit             : Quit this session.\n");
 }
