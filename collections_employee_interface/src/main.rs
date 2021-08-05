50381e8f2a37a6fbf8ba03b67a4e902527f525a9
Nathan Fox <nchristopherfox@gmail.com>
2021-08-05 19:09:06 -0400

    Add single employee storage capacity.
    There is now a HashMap structure holding the employee
    database. It allows one employee per department and
    implements both adding an employee and showing a department's
    employee.

--- collections_employee_interface/src/main.rs
+++ collections_employee_interface/src/main.rs
@@ -1,8 +1,10 @@
 use std::io::{self, Write};
+use std::collections::HashMap;
 
 fn main() {
     println!("Welcome to the Employee Database Interface.");
     print_valid_commands();
+    let mut database = HashMap::new();
     loop {
         let command = get_sentence();
         let split_command: Vec<&str> = command.split(" ").collect();
@@ -10,8 +12,8 @@ fn main() {
             continue
         }
         match split_command[0] {
-            "add" => add_employee(split_command),
-            "show" => show_department(split_command),
+            "add" => add_employee(split_command, &mut database),
+            "show" => show_department(split_command, &database),
             "commands" => print_valid_commands(),
             "quit" => break,
             _ => {
@@ -39,12 +41,27 @@ fn get_sentence() -> String {
     return sentence;
 }
 
-fn add_employee(split_command: Vec<&str>) {
-    println!("Adding employee with command \"{}\"", split_command.join(" "));
+fn add_employee(split_command: Vec<&str>, database: &mut HashMap<String, String>) {
+    let mut split_index = 0;
+    let split_index_ref = &mut split_index;
+    for (i, word) in split_command.iter().enumerate() {
+        if *word == "to" {
+            *split_index_ref = i;
+            break
+        }
+    }
+    let name = split_command[1..split_index].join(" ");
+    let department = split_command[split_index+1..].join(" ");
+    database.insert(department, name);
 }
 
-fn show_department(split_command: Vec<&str>) {
-    println!("Showing employees of department with command \"{}\"", split_command.join(" "));
+fn show_department(split_command: Vec<&str>, database: &HashMap<String, String>) {
+    let department = split_command[1..].join(" ");
+    let employee = match database.get(&department) {
+        Some(name) => name,
+        None => "",
+    };
+    println!("The employee in this department is {}", employee);
 }
 
 fn print_valid_commands() {
